48e2db17c2d29a98a77ead672fc7bd8af7eca002
AhuraMagda <lubaszka.magda@gmail.com>
2023-08-03 12:57:00 +0200

    show img in gallery

--- app.js
+++ app.js
@@ -52,12 +52,55 @@ const changeOpacity = (image) => {
     image.style.opacity = image.style.opacity === "1" ? ".5" : "1";
 }
 
+// GALLERY IMAGES - make img bigger and put it in the center
+
+const bigImageContainer = document.createElement('div')
+bigImageContainer.classList.add("big-image-container")
+
+const bigImage = document.createElement('img')
+bigImage.classList.add("big-image")
+
+const hideBigImageButton = document.createElement('span')
+hideBigImageButton.classList.add("hide-bigimage-button")
+
+const arrowLeft = document.createElement('span')
+arrowLeft.classList.add("arrow-left")
+
+const arrowRight = document.createElement('span')
+arrowRight.classList.add("arrow-right")
+
+const gallery = document.querySelector(".gallery-container")
+
+
+
 const showBigImage = (image) => {
-    return
+    // sprawdź czy zdjęcie już jest? jak tak to REMOVE a jak nie to
+    bigImage.src = image.src
+    gallery.appendChild(bigImageContainer)
+    bigImageContainer.appendChild(arrowLeft)
+    bigImageContainer.appendChild(bigImage)
+    bigImageContainer.appendChild(arrowRight)
+    bigImageContainer.appendChild(hideBigImageButton)
+    // albo jeśli jest tylko zmień bigImage.src = image.src, bardziej optymalnie?
+}
+
+const hideBigImage = () => {
+    gallery.removeChild(bigImageContainer)
+}
+
+const changeImg = () => {
+    console.log(bigImage.src) 
+    // sprawdź który to index w naszym array of images
+    // ustaw, że teraz ma być zdjęcie o indexie + 1 albo tam pierwszym
 }
 
 galleryImages.forEach(image => {
-    image.addEventListener("mouseover", function() {changeOpacity(image)})
-    image.addEventListener("mouseout", function() {changeOpacity(image)})
+    image.addEventListener("mouseover", function() {changeOpacity(image)});
+    image.addEventListener("mouseout", function() {changeOpacity(image)});
+    image.addEventListener("click", function() {showBigImage(image)});
     }
 )
+
+arrowRight.addEventListener("click", changeImg)
+
+hideBigImageButton.addEventListener("click", hideBigImage)
