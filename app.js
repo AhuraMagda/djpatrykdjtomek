28e10b94c7cc32f130d0de03ac676a5a90b38f29
AhuraMagda <104498979+AhuraMagda@users.noreply.github.com>
2023-08-15 21:50:11 +0200

    update

--- app.js
+++ app.js
@@ -3,44 +3,6 @@
 
 
 
-
-// HEADER text appearing
-
-const navLogo = document.querySelector(".nav__logo");
-
-const introTextUp1 = document.querySelector(".intro-text__up1");
-const introTextUp2 = document.querySelector(".intro-text__up2");
-const introTextUp3 = document.querySelector(".intro-text__up3");
-
-const introTextDown1 = document.querySelector(".intro-text__down1");
-const introTextDown2 = document.querySelector(".intro-text__down2");
-const introTextDown3 = document.querySelector(".intro-text__down3");
-
-
-const moveIntroText = () => {
-
-    setTimeout(() => {
-        introTextUp1.style.display = "block";
-        introTextUp2.style.display = "block";
-        introTextUp3.style.display = "block";
-        introTextDown1.style.display = "block";
-        introTextDown2.style.display = "block";
-        introTextDown3.style.display = "block";
-    }, "0")
-
-    setTimeout(() => {
-        introTextUp1.style.top = "65px";
-        introTextUp2.style.top = "30px";
-        introTextUp3.style.top = "0px";
-    
-        introTextDown1.style.top = "155px";
-        introTextDown2.style.top = "190px";
-        introTextDown3.style.top = "220px";
-    }, "1")
-}
-
-moveIntroText()
-
 // navLogo.addEventListener("click", moveIntroText)
 
 // GALLERY IMAGES - change opacity
