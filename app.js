2336c584bd8c52943926ce53e5817c1aab5cbe58
AhuraMagda <lubaszka.magda@gmail.com>
2023-11-03 21:19:40 +0100

    cleanup

--- app.js
+++ app.js
@@ -1,108 +1,120 @@
-
 // GALLERY IMAGES - change color
 
-const galleryImages = document.querySelectorAll(".gallery__img")
+const galleryImages = document.querySelectorAll(".gallery__img");
 
 const changeImgColor = (image) => {
-    image.classList.toggle("gray")
-}
+  image.classList.toggle("gray");
+};
 
-galleryImages.forEach(image => {
-    image.addEventListener("mouseover", function() {changeImgColor(image)});
-    image.addEventListener("mouseout", function() {changeImgColor(image)});
-    image.addEventListener("click", function() {showBigImage(image)});
-    }
-)
+galleryImages.forEach((image) => {
+  image.addEventListener("mouseover", function () {
+    changeImgColor(image);
+  });
+  image.addEventListener("mouseout", function () {
+    changeImgColor(image);
+  });
+  image.addEventListener("click", function () {
+    showBigImage(image);
+  });
+});
 
 // GALLERY IMAGES - make img bigger and put it in the center
 
-const bigImageContainer = document.createElement('div')
-bigImageContainer.classList.add("gallery__big-img__container")
+const bigImageContainer = document.createElement("div");
+bigImageContainer.classList.add("gallery__big-img__container");
 
-const bigImage = document.createElement('img')
-bigImage.classList.add("gallery__big-img")
+const bigImage = document.createElement("img");
+bigImage.classList.add("gallery__big-img");
 
-const hideBigImageButton = document.createElement('span')
-hideBigImageButton.classList.add("gallery__big-img__btn-hide")
+const hideBigImageButton = document.createElement("span");
+hideBigImageButton.classList.add("gallery__big-img__btn-hide");
 
-const arrowLeft = document.createElement('span')
-arrowLeft.classList.add("gallery__big-img__arrow-left")
+const arrowLeft = document.createElement("span");
+arrowLeft.classList.add("gallery__big-img__arrow-left");
 
-const arrowRight = document.createElement('span')
-arrowRight.classList.add("gallery__big-img__arrow-right")
+const arrowRight = document.createElement("span");
+arrowRight.classList.add("gallery__big-img__arrow-right");
 
-const gallery = document.querySelector(".gallery")
+const gallery = document.querySelector(".gallery");
 
 const showBigImage = (image) => {
-    bigImage.src = image.src
-    bigImage.id = image.id
-    gallery.appendChild(bigImageContainer)
-    bigImageContainer.appendChild(arrowLeft)
-    bigImageContainer.appendChild(bigImage)
-    bigImageContainer.appendChild(arrowRight)
-    bigImageContainer.appendChild(hideBigImageButton)
-}
+  bigImage.src = image.src;
+  bigImage.id = image.id;
+  gallery.appendChild(bigImageContainer);
+  bigImageContainer.appendChild(arrowLeft);
+  bigImageContainer.appendChild(bigImage);
+  bigImageContainer.appendChild(arrowRight);
+  bigImageContainer.appendChild(hideBigImageButton);
+};
 
 const hideBigImage = () => {
-    gallery.removeChild(bigImageContainer)
-}
+  gallery.removeChild(bigImageContainer);
+};
 
 const changeImgRight = () => {
-    let newId;
-    let lastIndex = galleryImages.length-1
-    galleryImages.forEach((image, index) => image.id === bigImage.id && (newId = (index === lastIndex ? 0 : index+1)));
-    bigImage.src = galleryImages[newId].src
-    bigImage.id = galleryImages[newId].id
-}
+  let newId;
+  let lastIndex = galleryImages.length - 1;
+  galleryImages.forEach(
+    (image, index) =>
+      image.id === bigImage.id && (newId = index === lastIndex ? 0 : index + 1)
+  );
+  bigImage.src = galleryImages[newId].src;
+  bigImage.id = galleryImages[newId].id;
+};
 
 const changeImgLeft = () => {
-    let newId;
-    let lastIndex = galleryImages.length-1
-    galleryImages.forEach((image, index) => image.id === bigImage.id && (newId = (index === 0 ? lastIndex : index-1)));
-    bigImage.src = galleryImages[newId].src
-    bigImage.id = galleryImages[newId].id
-}
+  let newId;
+  let lastIndex = galleryImages.length - 1;
+  galleryImages.forEach(
+    (image, index) =>
+      image.id === bigImage.id && (newId = index === 0 ? lastIndex : index - 1)
+  );
+  bigImage.src = galleryImages[newId].src;
+  bigImage.id = galleryImages[newId].id;
+};
 
-arrowRight.addEventListener("click", changeImgRight)
-arrowLeft.addEventListener("click", changeImgLeft)
+arrowRight.addEventListener("click", changeImgRight);
+arrowLeft.addEventListener("click", changeImgLeft);
 
-hideBigImageButton.addEventListener("click", hideBigImage)
+hideBigImageButton.addEventListener("click", hideBigImage);
 
 // HAMBURGER
-const navLinks = document.querySelectorAll("nav ul a")
-const burgerIcon = document.querySelector(".hamburger")
+const navLinks = document.querySelectorAll("nav ul a");
+const burgerIcon = document.querySelector(".hamburger");
 const mobileNav = document.querySelector("nav ul");
-const hamburgerTopBun = document.querySelector(".top-bun")
-const hamburgerBottomBun = document.querySelector(".bottom-bun")
-const hamburgerMeat = document.querySelector(".meat")
+const hamburgerTopBun = document.querySelector(".top-bun");
+const hamburgerBottomBun = document.querySelector(".bottom-bun");
+const hamburgerMeat = document.querySelector(".meat");
 
 const changeBurgerToX = () => {
-    hamburgerTopBun.classList.toggle("hamburger--top-bun_clicked")
-    hamburgerBottomBun.classList.toggle("hamburger--bottom-bun_clicked")
-    hamburgerMeat.classList.toggle("hamburger--meat_clicked")
-}
+  hamburgerTopBun.classList.toggle("hamburger--top-bun_clicked");
+  hamburgerBottomBun.classList.toggle("hamburger--bottom-bun_clicked");
+  hamburgerMeat.classList.toggle("hamburger--meat_clicked");
+};
 
-burgerIcon.addEventListener('click', function() {
-    mobileNav.classList.toggle('nav-active'); 
-    changeBurgerToX();
-})
-
+burgerIcon.addEventListener("click", function () {
+  mobileNav.classList.toggle("nav-active");
+  changeBurgerToX();
+});
 
 // nav hidding after click
 
-navLinks.forEach(link=>{link.addEventListener("click", () => {
-    mobileNav.classList.remove('nav-active');
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileNav.classList.remove("nav-active");
     changeBurgerToX();
-})})
-
+  });
+});
 
 // add copyright
 
-document.querySelector("#footer__date").innerText = `© ${new Date().getFullYear()} DJ\xA0Patryk\xA0DJ\xA0Tomek`;
+document.querySelector(
+  "#footer__date"
+).innerText = `© ${new Date().getFullYear()} DJ\xA0Patryk\xA0DJ\xA0Tomek`;
 
 // preloader
 
 const loader = document.querySelector("#preloader");
-window.addEventListener("load", function() {
-    loader.style.display = "none";
-})
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
